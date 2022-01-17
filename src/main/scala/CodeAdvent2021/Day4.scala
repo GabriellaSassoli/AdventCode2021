f9f137d8ce1e7dcc223e16a4cb8697a19bdfb65d
GabriellaSassoli <gabriellasassoli@gmail.com>
2022-01-17 17:11:51 +0000

    First attempt at day 5

--- src/main/scala/CodeAdvent2021/Day4.scala
+++ src/main/scala/CodeAdvent2021/Day4.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 
 object Day4 {
 
-  case class Boards(board: List[(List[Seq[(Int, Int)]], Int)]) // create 2 values second called boardIndex
+  case class MarkBoards(board: List[(List[Seq[(Int, Int)]], Int)]) // create 2 values second called boardIndex
   case class Winner(board: Seq[Seq[(Int, Int)]], lastExtractedNumber: Option[Int], winnerBoardNumber: Option[Int])
 
 
@@ -35,10 +35,10 @@ object Day4 {
    (resultPart1, resultPart2)
  }
 
-  def getLastWinningBoard(extractionNumbers: Seq[Int],boards: Boards, winnerBoardNumber: Int): Winner ={
+  def getLastWinningBoard(extractionNumbers: Seq[Int], boards: MarkBoards, winnerBoardNumber: Int): Winner ={
 
     val boardWinner = extractionWinner(extractionNumbers, boards, Seq.empty, -1)
-    val boardsWithoutWinner = Boards(boards.board.filter{boardMatrix => boardMatrix._2 != boardWinner.winnerBoardNumber.get})
+    val boardsWithoutWinner = MarkBoards(boards.board.filter{ boardMatrix => boardMatrix._2 != boardWinner.winnerBoardNumber.get})
 
     boardWinner.winnerBoardNumber match {
       case Some(boardNumber) =>{
@@ -61,31 +61,26 @@ object Day4 {
   }
 
   @tailrec
-  def extractionWinner(extractionNumbers:Seq[Int], boards: Boards, winningBoard: Seq[Int], lastExtractedNumber: Int ): Winner ={
+  def extractionWinner(extractionNumbers:Seq[Int], boards: MarkBoards, winningBoard: Seq[Int], lastExtractedNumber: Int ): Winner ={
       winningBoard match {
         case Nil => {
-                      if (extractionNumbers.tail == Nil){
-                        Winner(Seq.empty, None, None)
-                      }else {
-                        val markedBoards: Boards = mark(extractionNumbers.head, boards)
-                        extractionWinner(extractionNumbers.tail, markedBoards, getWinningBoard(markedBoards), extractionNumbers.head)
-                      }
+                      val markedBoards: MarkBoards = mark(extractionNumbers.head, boards)
+                      extractionWinner(extractionNumbers.tail, markedBoards, getWinningBoard(markedBoards), extractionNumbers.head)
                     }
         case List(winner) => Winner(boards.board(winner)._1, Some(lastExtractedNumber), Some(winner))
       }
   }
 
-  def getWinningBoard(boards: Boards): Seq[Int] = // checking only that the line is marked and not the column as well hence why I have the wrong answer for part 2.
+  def getWinningBoard(boards: MarkBoards): Seq[Int] = // checking only that the line is marked and not the column as well hence why I have the wrong answer for part 2.
     boards.board.collect{
       boardMatrix => boardMatrix._1
-        .map{ line => line.map(value => value._2)}
-        .map{value => if(value.sum == 5) Some(boardMatrix._2) else None}
-        .flatten
+        .map { line => line.map(value => value._2) }
+        .flatMap { value => if (value.sum == 5) Some(boardMatrix._2) else None }
     }.flatten
 
 
-  def mark(extractedNumber: Int, boards: Boards): Boards = {
-      Boards(boards.board.map{boardMatrix =>
+  def mark(extractedNumber: Int, boards: MarkBoards): MarkBoards = {
+      MarkBoards(boards.board.map{ boardMatrix =>
         boardMatrix._1.map{line =>
           line.map{value =>
             if(value._1 == extractedNumber) value.copy(_2 = 1)
@@ -96,15 +91,15 @@ object Day4 {
   }
 
   def getExtractionNumber(input: List[String]): List[Int]={
-    input.splitAt(1)._1.map(_.split(",")).flatten.map{_.toInt}
+    input.splitAt(1)._1.flatMap(_.split(",")).map{_.toInt}
   }
 
-  def getBoards(input: List[String]): Boards = {
+  def getBoards(input: List[String]): MarkBoards = {
 
     val noSelection = Array.fill(input.length * 5)(0)
-    val boards: Seq[Int] = input.splitAt(1)._2.filter(_.size>0).map(_.split(" +").toList.map(_.toIntOption).flatten).flatten
+    val boards: Seq[Int] = input.splitAt(1)._2.filter(_.size > 0).flatMap(_.split(" +").toList.flatMap(_.toIntOption))
 
-    Boards(boards
+    MarkBoards(boards
       .grouped(25)
       .map{ lines => lines.grouped(5)
         .map{
@@ -117,7 +112,7 @@ object Day4 {
 object Day4Main {
 
   def main(args: Array[String]): Unit = {
-    val input = getInput("src/main/resources/Day4Input")
+    val input = getInput("src/main/resources/2021/Day4Input")
     println(solution(input))
   }
 
--- src/main/scala/CodeAdvent2021/Day5.scala
+++ src/main/scala/CodeAdvent2021/Day5.scala
@@ -1,23 +1,95 @@
 package CodeAdvent2021
 
+
+
 import scala.io.Source
 
 
 object Day5 {
 
-//  // Steps 1 Read input using regex
-//  // Step 2 put input into (x,y) case class
-//  def main(args: Array[String]): Unit = {
-//    val input: List[String] = readInput("src/main/resources/Day3Input")
-////    println(solution(input))
-////    println(solutionPart2(input))
-//  }
-//
-//  def readInput(filepath: String) =
-//    Source.fromResource(filepath).getLines.toList
-//
-//  // manipulating input
-//  val lineRegex = "(\\w+) (\\d+)".r
-//  case class Movement(direction: String, step: Int)
+  val lineRegex = "(\\d+),(\\d+) -> (\\d+),(\\d+)".r
 
+  case class Coordinates(x: Int, y: Int)
+  type Grill = Array[Array[Int]]
+  type VentilatorCoordinates = Seq[(Coordinates, Coordinates)]
+
+  // Step 1: Read input using regex
+  // Step 2: create Grill
+  // Step 3: put input into (x,y, value) case class
+  // Step 4: find max value of value field
+  // Step 5: find how many time number is present.
+
+
+  def main(args: Array[String]): Unit = {
+
+    val input: Seq[String] = readInput("2021/Day5Input.txt")
+    val coordinates: VentilatorCoordinates = grillCoordinates(input)
+    val initialGrill = createGrill(coordinates)
+    val coordinatesToConsider = coordinatesConsidered(coordinates)
+    val grill = fillGrill(initialGrill,coordinatesToConsider)
+    println(getOverlapsPoints(grill))
+
+  }
+
+  def readInput(filepath: String): Seq[String] =
+    Source.fromResource(filepath).getLines.toList
+
+  // manipulating input
+
+  def parse(line: String): (Coordinates,Coordinates) =
+    line match {
+      case lineRegex(x1, y1, x2,y2) => (Coordinates(x1.toInt,y1.toInt), Coordinates(x2.toInt, y2.toInt))
+    }
+
+  def grillCoordinates(input:Seq[String]): VentilatorCoordinates = {
+    input
+      .map{ line => parse(line)}
+  }
+
+  def createGrill(coordinates: Seq[(Coordinates, Coordinates)]): Grill = {
+
+    //could start empty
+    val rows = coordinates.flatMap(coordinates => List(coordinates._1.x, coordinates._2.x)).max
+    val cols = coordinates.flatMap(coordinates => List(coordinates._1.y, coordinates._2.y)).max
+
+    Array.ofDim[Int](rows + 1, cols + 1)
+
+  }
+
+  def fillGrill(grill: Grill, fillingCoordinates: VentilatorCoordinates ): Grill ={
+    //fillingCoordinates are already filtered coordinates
+    fillingCoordinates.collect {
+      case coordinates if coordinates._1.x == coordinates._2.x  && coordinates._2.y > coordinates._1.y =>
+        (coordinates._1.y to coordinates._2.y)
+          .foreach(cell => grill(coordinates._1.x)(cell) = add(grill,Coordinates(coordinates._1.x,cell)))
+      case coordinates if coordinates._1.x == coordinates._2.x  && coordinates._1.y > coordinates._2.y =>
+        (coordinates._2.y to coordinates._1.y)
+          .foreach(cell => grill(coordinates._1.x)(cell) = add(grill,Coordinates(coordinates._1.x,cell)))
+      case coordinates if coordinates._1.y == coordinates._2.y  && coordinates._2.x > coordinates._1.x =>
+        (coordinates._1.x to coordinates._2.x)
+          .foreach(cell => grill(cell)(coordinates._1.y) = add(grill,Coordinates(cell,coordinates._1.y)))
+      case coordinates if coordinates._1.y == coordinates._2.y && coordinates._1.x > coordinates._2.x =>
+        (coordinates._2.x to coordinates._1.x)
+          .foreach(cell => grill(cell)(coordinates._1.y) = add(grill,Coordinates(cell,coordinates._1.y)))
+    }
+    grill.transpose
+  }
+
+  def coordinatesConsidered(grillCoordinates: VentilatorCoordinates ): VentilatorCoordinates ={
+    grillCoordinates.filter{
+      coordinate =>
+        (coordinate._1.x == coordinate._2.x || coordinate._1.y == coordinate._2.y)
+    }
+  }
+
+  def add(grill: Grill, coordinates: Coordinates): Int ={
+    grill(coordinates.x)(coordinates.y) match {
+      case 0 =>  1
+      case value => value + 1
+    }
+  }
+
+  def getOverlapsPoints(grill:Grill) ={
+    grill.flatMap(value => value.filter(_ > 1)).length
+  }
 }
