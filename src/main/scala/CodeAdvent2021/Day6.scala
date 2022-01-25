3188f953ccc4ec677a9ee5105109e137a6e32f27
GabriellaSassoli <gabriellasassoli@gmail.com>
2022-01-25 22:19:58 +0000

    day 6 part 2

--- src/main/scala/CodeAdvent2021/Day6.scala
+++ src/main/scala/CodeAdvent2021/Day6.scala
@@ -1,26 +1,33 @@
 package CodeAdvent2021
 
-import CodeAdvent2021.Day5.{filterCoordinates, getVentilatorsPosition, parse, readInput}
-
-import scala.::
 import scala.io.Source
 
 object Day6 {
 
   type Lanternfishes = Seq[Int]
+  type LanternfishNumber = BigInt
+  type RemainingDays = Int
 
   def main(args: Array[String]): Unit = {
     val input: Lanternfishes = readInput("2021/Day6Input.txt")
-    println(input)
-    println(lanternfishCycle(input,0,80).size)
+    println(s"Lantern fish after 80 days: Method1 ${lanternfishCycle(input,0,80).size}, Method2 ${sumMapValues(lanternfisheGrowpart2(createLanternfishMap(input),0,80))}")
+    println(s"lanternfish after 256 days ${sumMapValues(lanternfisheGrowpart2(createLanternfishMap(input),0,256))}")
   }
 
-  // idea recursive function that takes one -1 off except if 0 then becomes 6 and adds a new entry.
+  def sumMapValues(lanternfishMap: Map[RemainingDays, LanternfishNumber]): BigInt = {
+    lanternfishMap.collect{
+      case (_,value) => value
+    }.sum
+  }
+
   def readInput(filepath: String): Lanternfishes =
     Source.fromResource(filepath).getLines.toList.flatMap(lanternfish=> lanternfish.split(",")).map(_.toInt)
 
+  def createLanternfishMap(lanternfishes: Lanternfishes): Map[RemainingDays, LanternfishNumber] =
+    lanternfishes.groupBy(identity).mapValues(value => BigInt(value.size)).toMap
+
   //returns the new fishes
-  def lanternfishCycle(lanternfishes: Lanternfishes, day: Int, nDaysTotal: Int): Seq[Int] ={
+  def lanternfishCycle(lanternfishes: Lanternfishes, day: Int, nDaysTotal: Int): Seq[Int] = {
     if (day < nDaysTotal){
       val newLanternfishes: Seq[Int] = (0 to (lanternfishes.length - 1)).flatMap{
          lanternfishIndex =>
@@ -36,5 +43,22 @@ object Day6 {
       lanternfishes
     }
 
+  }
+
+  def lanternfisheGrowpart2(lanternfishes: Map[RemainingDays,LanternfishNumber], day: Int, nDaysTotal: Int): Map[RemainingDays, LanternfishNumber] = {
+    if (day < nDaysTotal){
+      lanternfisheGrowpart2(newLanternFishMap(lanternfishes),day + 1, nDaysTotal)
+    }
+    else{
+      lanternfishes
+    }
+  }
+
+  def newLanternFishMap(lanternfishes: Map[RemainingDays, LanternfishNumber]): Map[RemainingDays, LanternfishNumber] = {
+    lanternfishes.flatMap{
+      case (dayLeft0, nFish) if dayLeft0 == 0 => Map((6,nFish),(8,nFish))
+      case (dayLeft7, nFish7) if dayLeft7 == 7 => Map((6,nFish7 + lanternfishes.get(0).getOrElse(0)))
+      case (dayLeft, nFish) => Map((dayLeft - 1,nFish))
+    }
   }
 }
--- src/test/scala/CodeAdvent2021/Day6Test.scala
+++ src/test/scala/CodeAdvent2021/Day6Test.scala
@@ -2,6 +2,8 @@ package CodeAdvent2021
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.collection.MapView
+
 class Day6Test extends FlatSpec with Matchers{
 
   val testFilePath = "test-input-6"
@@ -18,5 +20,29 @@ class Day6Test extends FlatSpec with Matchers{
     val newLanternfish = Day6.lanternfishCycle(lanternFish,0,18)
     newLanternfish.size shouldBe 26
     newLanternfish shouldBe Seq(6, 8, 1, 1, 3, 0, 2, 2, 4, 6, 8, 1, 1, 3, 4, 6, 6, 8, 6, 8, 1, 5, 7, 0, 0, 2)
+  }
+
+  behavior of "createLanternfishMap"
+  it should "return a map of lanternfishes" in {
+    val lanternFish = Seq(3,4,3,1,2)
+    Day6.createLanternfishMap(lanternFish) shouldBe Map((1,1), (2,1), (3,2), (4,1))
+  }
+
+  behavior of "newLanternFishMap"
+  it should "return a map of lanternfishes" in {
+    Day6.newLanternFishMap(Map((1,1), (2,1), (3,2), (4,1))) shouldBe Map((0,1), (1,1), (2,2), (3,1))
+    Day6.newLanternFishMap(Map((0,1), (1,1), (2,2), (3,1))) shouldBe Map((6,1), (0,1), (1,2), (2,1),(8,1))
+    Day6.newLanternFishMap(Map((0,1), (7,1), (2,2), (3,1))) shouldBe Map((6,2),(8,1),(1,2), (1,2), (2,1))
+  }
+
+  behavior of "lanternfisheGrowpart2"
+  it should "return the fish after n days" in {
+    Day6.lanternfisheGrowpart2(Map((1,1), (2,1), (3,2), (4,1)),0,18) shouldBe Map(0 -> 3, 5 -> 1, 1 -> 5, 6 -> 5, 2 -> 3, 7 -> 1, 3 -> 2, 8 -> 4, 4 -> 2)
+
+  }
+
+  behavior of "sumMapValues"
+  it should "return the sum of the map values" in {
+    Day6.sumMapValues(Map((1,1), (2,1), (3,2), (4,1))) shouldBe 5
   }
 }
